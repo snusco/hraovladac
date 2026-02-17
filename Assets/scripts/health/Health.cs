d3416aa8447edac27a7a0ff2f58fd7b59986c9fe
Winlex1 <kaalex822@gmail.com>
2026-02-17 23:15:39 +0100

    Dalsia verzia

--- Assets/scripts/health/Health.cs
+++ Assets/scripts/health/Health.cs
@@ -3,18 +3,24 @@ using System.Collections;
 
 public class Health : MonoBehaviour
 {
-    [Header ("Health")]
+    [Header("Health")]
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
     private Animator anim;
     private bool dead;
-   
-    [Header ("Iframes")]
-    [SerializeField]private float iframesDuration;
-    [SerializeField]private int numberOFFlashes;
+
+    [Header("iFrames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numberOfFlashes;
     private SpriteRenderer spriteRend;
 
+    [Header("Components")]
+    [SerializeField] private Behaviour[] components;
+    private bool invulnerable;
+
+    [Header("Death Sound")]
     [SerializeField] private AudioClip deathSound;
+    [SerializeField] private AudioClip hurtSound;
 
     private void Awake()
     {
@@ -22,51 +28,66 @@ public class Health : MonoBehaviour
         anim = GetComponent<Animator>();
         spriteRend = GetComponent<SpriteRenderer>();
     }
-
     public void TakeDamage(float _damage)
     {
+        if (invulnerable) return;
         currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
 
         if (currentHealth > 0)
         {
-          
             anim.SetTrigger("hurt");
             StartCoroutine(Invunerability());
-             
-        }    
-
+        }
         else
         {
             if (!dead)
-            {   
-                if (GetComponent<PlayerMovement>() != null)
-                    GetComponent<PlayerMovement>().enabled = false;
+            {
+                //Deactivate all attached component classes
+                foreach (Behaviour component in components)
+                    component.enabled = false;
 
                 anim.SetBool("grounded", true);
                 anim.SetTrigger("die");
-
-                SoundManager.Instance.PlaySound(deathSound);
 
                 dead = true;
             }
         }
     }
-
     public void AddHealth(float _value)
     {
         currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
     }
-
     private IEnumerator Invunerability()
-    { 
-      Physics2D.IgnoreLayerCollision(10,11, true) ;
-        for(int i = 0; i < numberOFFlashes; i++) 
+    {
+        invulnerable = true;
+        Physics2D.IgnoreLayerCollision(10, 11, true);
+        for (int i = 0; i < numberOfFlashes; i++)
         {
-        spriteRend.color = new Color(1,0,0, 0.5f);
-        yield return new WaitForSeconds(iframesDuration / (numberOFFlashes * 2));
-         spriteRend.color = Color .white;
-        yield return new WaitForSeconds(iframesDuration / (numberOFFlashes * 2));
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
         }
-      Physics2D.IgnoreLayerCollision(10,11, false) ;
+        Physics2D.IgnoreLayerCollision(10, 11, false);
+        invulnerable = false;
+    }
+    private void Deactivate()
+    {
+        gameObject.SetActive(false);
+    }
+
+    //Respawn
+    public void Respawn()
+    {
+        dead = false;
+
+        AddHealth(startingHealth);
+        anim.ResetTrigger("die");
+        anim.Play("idle");
+        StartCoroutine(Invunerability());
+
+        //Activate all attached component classes
+        foreach (Behaviour component in components)
+            component.enabled = true;
     }
 }
